6a4cdac33e19cc165a5299169f976299fda7a8d3
Stephanie Marsh <adamsstephaniem@gmail.com>
2021-07-31 15:21:38 -0400

    image src

--- src/imageList.jsx
+++ src/imageList.jsx
@@ -1,14 +1,20 @@
 import { render } from 'preact';
 
+const envPublicUrl = {
+  production: 'https://earthtone0ne.github.io/freecycleGallery/',
+  development: '../public',
+}
+
 function ImageItem(source, description) {
-  const imgSource = `${process.env.PUBLIC_URL || 'public'}/${source}`;
-	return <li class="image-item">
-      <img src={imgSource} alt={description} onClick={() => window.open(imgSource)} />
+  const imgSource = `${envPublicUrl[process.env.NODE_ENV || 'development']}/${source}`;
+	const returnValue = <li class="image-item">
+    <img src={imgSource} alt={process.env.PUBLIC_URL || 'nothin'} onClick={() => window.open(imgSource)} />
   </li>
 }
 
 
 function ImageList({images, description}) {
+  console.log(process.env)
   return <ul class="image-list">
     {images.map(image => ImageItem(image, description))}
   </ul>
